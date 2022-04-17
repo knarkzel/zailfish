531ad9578cd4b4782637ad67839d70caf9714c89
Knarkzel <knarkzel@gmail.com>
2022-04-17 05:24:22 +0200

    it works better now

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const print = std.debug.print;
 const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
+const ArenaAllocator = std.heap.ArenaAllocator;
+const ComptimeArrayList = @import("comptime-arraylist.zig").ComptimeArrayList;
 
 const Token = union(enum) { string: []const u8, block: []const u8 };
 
@@ -20,67 +22,52 @@ fn block(input: []const u8) ?Token {
     return null;
 }
 
-fn parse(allocator: Allocator, input: []const u8) !ArrayList(Token) {
-    var tokens = ArrayList(Token).init(allocator);
-    var cursor: usize = 0;
-    while (cursor < input.len) {
-        if (block(input[cursor..])) |token| {
-            cursor += token.block.len;
-            const trimmed = std.mem.trim(u8, token.block, " \n{}");
-            try tokens.append(Token{ .block = trimmed });
-        } else if (string(input[cursor..])) |token| {
-            cursor += token.string.len;
-            try tokens.append(token);
-        } else break;
+fn parse(comptime input: []const u8) []const Token {
+    comptime {
+        var cursor: usize = 0;
+        var tokens: []const Token = &.{};
+        while (cursor < input.len) {
+            if (block(input[cursor..])) |token| {
+                cursor += token.block.len;
+                const result = Token{ .block = std.mem.trim(u8, token.block, " \n{}") };
+                tokens = tokens ++ [_]Token{result};
+            } else if (string(input[cursor..])) |token| {
+                cursor += token.string.len;
+                tokens = tokens ++ [_]Token{token};
+            } else break;
+        }
+        return tokens;
     }
-    return tokens;
 }
 
-fn template(allocator: Allocator, input: []const u8, comptime context: type, runtime: anytype) ![]const u8 {
-    const tokens = try parse(allocator, input);
-    var output = ArrayList(u8).init(allocator);
-    tokens: for (tokens.items) |token| switch (token) {
-        .block => |inner| {
-            // Find matching field in context then pass it
-            inline for (@typeInfo(context).Struct.fields) |field| {
-                if (std.mem.eql(u8, field.name, inner)) {
-                    const result = try std.fmt.allocPrint(allocator, "{s}", .{@field(runtime, field.name)});
-                    try output.appendSlice(result);
-                    continue :tokens;
-                }
-            }
-        },
-        .string => |inner| try output.appendSlice(inner),
-    };
-    return std.fmt.allocPrint(allocator, "\n{s}\n", .{output.items});
+fn template(input: []const u8, comptime context: type) fn (Allocator, anytype) anyerror![]const u8 {
+    const tokens = parse(input);
+    return struct {
+        fn result(allocator: Allocator, runtime: anytype) anyerror![]const u8 {
+            var output = ArrayList(u8).init(allocator);
+            tokens: for (tokens) |token| switch (token) {
+                .block => |inner| {
+                    // Find matching field in context then pass it
+                    inline for (@typeInfo(context).Struct.fields) |field| {
+                        if (std.mem.eql(u8, field.name, inner)) {
+                            const slice = try std.fmt.allocPrint(allocator, "{s}", .{@field(runtime, field.name)});
+                            try output.appendSlice(slice);
+                            continue :tokens;
+                        }
+                    }
+                },
+                .string => |inner| try output.appendSlice(inner),
+            };
+            return std.fmt.allocPrint(allocator, "\n{s}\n", .{output.items});
+        }
+    }.result;
 }
 
 test "template" {
-    // Allocator
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
-    // Code
+    // Create template function then call it and print
+    var allocator = ArenaAllocator.init(std.heap.page_allocator).allocator();
     const input = @embedFile("../templates/index.html");
-    const Index = struct { name: []const u8 };
-    const index = Index{ .name = "hello bro" };
-    const output = try template(allocator, input, Index, index);
-    print("\n{s}\n", .{output});
-}
-
-test "parser" {
-    // Allocator
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
-    // Code
-    const input = @embedFile("../templates/index.html");
-    const tokens = try parse(allocator, input);
-    print("\n", .{});
-    for (tokens.items) |token| switch (token) {
-        .block => |inner| print("Block({s})\n", .{inner}),
-        .string => |inner| print("String({s})\n", .{inner}),
-    };
+    const index_template = template(input, struct { name: []const u8 });
+    const result = try index_template(allocator, .{ .name = "hello bro" });
+    print("{s}", .{result});
 }
